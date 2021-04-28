5b530bf6ebea70ac9886ac4c1a4b603867ecc699
franklyT <48483484+franklyT@users.noreply.github.com>
2021-04-28 02:24:40 -0400

    Update App.tsx

--- src/groups/App.tsx
+++ src/groups/App.tsx
@@ -1,9 +1,9 @@
 import './App.module.scss';
 import HeaderComponent from './Header/HeaderComponent';
 import Sidebar from "./Sidebar/SidebarComponent";
+import CardContainer from './Card/CardContainer';
 
 import styles from './App.module.scss';
-import CardContainer from './Card/CardContainer';
 
 function App() {
   return (
